a86a2d0a6b5a387716ab85777a6b16f087c3d3dc
csvillalta <christophervillalta@gmail.com>
2018-12-21 05:43:22 -0500

    Modify generate script and add sample generations

--- generate.py
+++ generate.py
@@ -4,14 +4,19 @@ from random import randint
 title_generator = textgenrnn('title_generator_weights.hdf5')
 instruction_generator = textgenrnn('instruction_generator_weights.hdf5')
 
-recipe_lines = randint(10,30)
-generated_title = title_generator.generate(n=1,
-                                            max_gen_length=50,
-                                            return_as_list=True)
-generated_instructions = instruction_generator.generate(n=recipe_lines,
-                                                max_gen_length=1000,
+for i in range(30):
+    recipe_lines = randint(10,30)
+    generated_title = title_generator.generate(n=1,
+                                                max_gen_length=50,
                                                 return_as_list=True)
+    generated_instructions = instruction_generator.generate(n=recipe_lines,
+                                                    max_gen_length=1000,
+                                                    return_as_list=True)
 
-print("Title: {}".format("".join(generated_title)))
-print("Instructions:")
-print(" ".join(generated_instructions))
+    title = "Title: {}".format("".join(generated_title))
+    file_name = "samples/recipe_sample_{}.txt".format(i)
+    with open(file_name, "w+") as f:
+        f.write(title)
+        f.write("\n\n")
+        f.write("Instructions: ")
+        f.writelines(generated_instructions)
